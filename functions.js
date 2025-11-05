44f970046f04e26a0a8dbef81844b17f5bfbd7be
vineet dixit <vineetdixit727@gmail.com>
2025-11-05 19:22:51 +0530

    still learning js

--- functions.js
+++ functions.js
@@ -2,4 +2,17 @@
 // This file contains utility functions for various operations.
 
 //so now we will create a function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+//another way
+const multiplyNumbers = (a, b) => {
+    return a * b;
+}
+//another way
+const divideNumbers = (a, b) => a / b;
+
+
+
 
