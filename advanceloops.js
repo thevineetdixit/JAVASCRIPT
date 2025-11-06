fd5322af4ab2f0576ae9c2c04a9fc89f8b1f8aa7
vineet dixit <vineetdixit727@gmail.com>
2025-11-06 19:31:51 +0530

    learnt about advanced loops in js

---
+++ advanceloops.js
@@ -0,0 +1,29 @@
+let obj = {
+    name : "Vineeth",
+    age : 21,
+    city : "Chennai",
+    gender : "male",
+}
+
+
+//for in loop - to iterate over keys(property) of an object
+for(let key in obj){
+    console.log(key); //prints keys
+    console.log(obj[key]); //prints values
+}
+
+//now we can also use Object.keys() method to get an array of keys and then use for of loop to iterate over them
+//so now we weill learn about the diff in getting keys with for in and Object.keys()
+
+let obj2 = Object.create({obj});
+obj2.money = 420;
+obj2.id = "vineet";
+console.log(obj2);//prints only own properties money and id because obj is in prototype chain
+//it can access the properties of obj but they are not its own properties
+console.log(obj.name);//prints Vineeth
+console.log(obj.city);//prints Chennai
+//but its own properties are money and id
+
+
+
+
