c519780d4ab096a72a927e4814d96c5236e9813f
vineet dixit <vineetdixit727@gmail.com>
2025-11-06 17:50:10 +0530

    still learning js

--- ifelse.js
+++ ifelse.js
@@ -56,5 +56,3 @@
         default:
             console.log("Invalid day");
     }
-
-    
---
+++ loops.js
@@ -0,0 +1,22 @@
+//for loop
+for(let i=1; i<=5; i++){
+    console.log("Iteration number: " + i);
+}   
+
+
+
+//while loop
+let j = 1;
+while(j <= 5){
+    console.log("While loop iteration: " + j);
+    j++;
+}
+
+
+//nested loops
+for(let a=1; a<=3; a++){
+    for(let b=1; b<=2; b++){
+        console.log("a: " + a + ", b: " + b);
+    }
+}
+
