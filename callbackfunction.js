0b3c0033d333e101c5c32c42a3482fddaee9be7d
vineet dixit <vineetdixit727@gmail.com>
2025-11-12 00:10:56 +0530

    learnt about filter and callback

--- callbackfunction.js
+++ callbackfunction.js
@@ -46,4 +46,31 @@ function fetchdata(){
 //        fetching data from server...
 // here fetchdata function is called every 2 seconds by setInterval function
 
-//commit paglu
+
+
+//forEach loop
+let arrr = [1,2,3,4,5]
+arrr.forEach(function(num){
+    console.log(num)
+});
+//output: 1 2 3 4 5
+// here forEach loop takes a callback function as an argument and calls it for each element in the array
+// you can also use arrow function
+arrr.forEach( (num,index) => console.log(num,index ));
+arrr.forEach( (num,index,nums) => nums[index] = num * 2 );// modifying original array
+//output: 2 4 6 8 10
+
+
+const pri = (num) => console.log(num);
+arrr.forEach(pri);
+//output: 2 4 6 8 10
+// here pri function is passed as a callback to forEach loop
+
+//filter 
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const evenNumbers = numbers.filter( (num) => num % 2 === 0 );//no need to defien return keyword in arrow function if its a single line
+console.log(evenNumbers);
+//output: [2,4,6,8,10]
+// here filter method takes a callback function that returns true for even numbers and creates a new array with those numbers only  
+
+
